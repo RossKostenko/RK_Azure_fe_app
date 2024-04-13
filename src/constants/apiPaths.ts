420d42c7e7d45606dcbb5ac7f788e42a17ca5f38
Rostyslav_Kostenko <rkostenko@slb.com>
2024-04-13 19:20:21 +0300

    feat(task-3): updated ednpoint

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,5 +1,5 @@
 const API_PATHS = {
-  product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  product: "https://rkg-fa-products-service-ne-001.azurewebsites.net/api",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
